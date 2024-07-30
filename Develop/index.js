f7c77c581caaf579774f8f8233317e744eebbe16
Phy <pham.phy@gmail.com>
2024-07-30 12:40:04 -0500

    cleaning up code

--- Develop/index.js
+++ Develop/index.js
@@ -9,6 +9,7 @@ const questions = ({projectName, description, installation, usage, guidelines, t
 
 ## Description
 Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+<br />
 ${description}
 
 - What was your motivation?
@@ -28,12 +29,15 @@ If your README is long, add a table of contents to make it easy for users to fin
 ## Installation
 
 What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+<br />
 ${installation}
-
+<br />
 ## Usage
 
 Provide instructions and examples for use. Include screenshots as needed.
+<br />
 ${usage}
+<br />
 To add a screenshot, create an 'assets/images' folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
 
     'md
@@ -47,14 +51,17 @@ List your collaborators, if any, with links to their GitHub profiles.
 If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
 
 If you followed tutorials, include links to those here as well.
+<br />
 [${github}](${github})
-
+<br />
 [${email}](${email})
-
+<br />
 ## License
 
 The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+<br />
 ${license}
+<br />
 ---
 
 🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
@@ -72,11 +79,13 @@ If your project has a lot of features, list them here.
 ## How to Contribute
 
 If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+<br />
 ${guidelines}
-
+<br />
 ## Tests
 
 Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+<br />
 ${test}
 `;
 
@@ -85,19 +94,6 @@ ${test}
     // TODO: Create a function to write README file
 function writeToFile(data) {
 fs.writeFile(`readme.md`, data, (err) => err ? console.error(err) : console.log('Success!'))};
-
-
-
-//selecting from the license's list above in inquirer and takes you to the license URL
-//const inquirer = require('inquirer');
-// const open = require('open');  //to open URLs
-// const licenses = [
-//     {
-//         name: 'MIT', value: 'https://choosealicense.com/licenses/mit/',
-//         name: 'GNU', value: 'https://choosealicense.com/licenses/agpl-3.0/',
-//         name: 'Apache', value:'https://choosealicense.com/licenses/apache-2.0/'
-//     }
-// ];
 
 
 // TODO: Create a function to initialize app
